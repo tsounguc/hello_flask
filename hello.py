4fcab9c93fcdb56c85cb9d9569930aebdda74861
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-21 15:31:41 -0500

    minor changes

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask
-
+# Initial Flask application
 app = Flask(__name__)
 
 print(__name__)
@@ -9,5 +9,6 @@ print(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# this does the same thing as the command line flask --app  hello run
 # if __name__ == "__main__":
 #     app.run()
