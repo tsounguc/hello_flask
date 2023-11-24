42472bd43f47e76cb4470292cff373ff6bb6715c
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-23 20:32:39 -0500

    rendinging html elements with flask

--- hello.py
+++ hello.py
@@ -7,7 +7,9 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return '<h1 style="text-align: center">Happy Thanksgiving!</h1>' \
+            '<p style="text-align: center">Yay Vegans!</p>' \
+            '<img src="https://media0.giphy.com/media/m6ghJgaPsA8WgMccSm/giphy.gif?cid=ecf05e47s66zqbv351llv9q8rhea4kswp0v8usu8wbvj1zf2&ep=v1_gifs_search&rid=giphy.gif&ct=g" width=200>'
 
 @app.route("/bye")
 def bye():
